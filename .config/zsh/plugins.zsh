b8cea4959c00770c78f286b907044ecb2dbba63f
s1gint-z3r0 <s1gint.z3r0.ops@proton.me>
2026-01-30 22:11:47 +0100

    try zinit plugin manager

---
+++ .config/zsh/plugins.zsh
@@ -0,0 +1,4 @@
+# trying zinit instead of oh-my-zsh , not decided yet
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+zinit light zsh-users/zsh-autosuggestions
+zinit light zsh-users/zsh-syntax-highlighting
